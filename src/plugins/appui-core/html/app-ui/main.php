6b2dcc4d9a87ef11268dc39f16cc38ff19f15a8f
nabab <thomas.nabet@gmail.com>
2025-09-19 11:50:11 +0200

    Fix sur clipboard

--- src/plugins/appui-core/html/app-ui/main.php
+++ src/plugins/appui-core/html/app-ui/main.php
@@ -1,8 +1,8 @@
   <!-- CLIPBOARD SLIDER -->
 <div>
   <bbn-clipboard :storage="true"
-                  ref="clipboard"
-                  @copy="onCopy"
-                  :z-index="13"
-                  style="overflow: visible;"/>
+                 ref="clipboard"
+                 @copy="onCopy"
+                 :z-index="13"
+                 style="overflow: visible;"/>
 </div>
